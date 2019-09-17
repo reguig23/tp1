99b27789f4d210330c620c578627962bca6cfaac
ireguig <ireguig@pcalb-mm0622.univ-jfc.fr>
2019-09-17 14:55:44 +0200

    Modif apporter ajoue d'une variable

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -15,7 +15,8 @@ public class JavaApplication1 {
         return a+b+6*15; /*fait une addition */
     }
      static int  LOL;
-     int marche=100;
+     int marche=19;
+     
     /* hihihiihihihihihi**/
         
     /**
